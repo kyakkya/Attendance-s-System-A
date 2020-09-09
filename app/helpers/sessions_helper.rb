c711f24f369b9c185bf0aaeeb2a70f332a30fdbc
kyakkya <inataaiko123@gmail.com>
2020-09-09 13:31:25 +0000

    pagenate終了

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -17,6 +17,7 @@ module SessionsHelper
     cookies.delete(:remember_token)
   end
   
+   # セッションと@current_userを破棄します
   def log_out
     forget(current_user)
     session.delete(:user_id)
@@ -36,9 +37,24 @@ module SessionsHelper
     end
   end
   
+   # 渡されたユーザーがログイン済みのユーザーであればtrueを返します。。
+  def current_user?(user)
+    user == current_user
+  end
+  
    # 現在ログイン中のユーザーがいればtrue、そうでなければfalseを返します。
   def logged_in?
     !current_user.nil?
+  end
+  
+  def redirect_back_or(default_url)
+    redirect_to(session[:forwarding_url] || default_url)
+    session.delete(:forwarding_url)
+  end
+  
+   # アクセスしようとしたURLを記憶します。
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
  
 end
