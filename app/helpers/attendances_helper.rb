8a0fe9818588bd985236b20264963a0c817116ed
kyakkya <inataaiko123@gmail.com>
2020-09-20 19:15:18 +0000

    attendance.helper.rb, show,toの編集

--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -11,5 +11,13 @@ module AttendancesHelper
   
   def working_times(start, finish)
     format("%.2f", (((finish - start) / 60) / 60.0))
-  end  
+  end
+  
+  def format_hour(time)
+   format("%02d", time.hour)
+  end
+ 
+  def format_min(time)
+    format("%02d", ((time.min) / 15) * 15)
+  end      
 end
