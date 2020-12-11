4a90661f2ec9ebb1ad10221c6d6af67c07b0efe8
kyakkya <inataaiko123@gmail.com>
2020-12-11 16:32:53 +0000

    時間の設定

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,14 +1,17 @@
 module UsersHelper
- def format_basic_info(time)
-   format("%.2f", ((time.hour * 60) + time.min) / 60.0)
- end
+  def format_basic_info(time)
+    format("%.2f", ((time.hour * 60) + time.min) / 60.0)
+  end
  
- def format_overtime(time)
-   format("%.2f", ((time.hour * 60) + time.min) / 60.0)
- end   
+ 
+  def format_overtime(overtime, end_time, next_day)
+    if next_day == true
+      format("%.2f", (overtime.hour - end_time.hour) + ((overtime.min - end_time.min) / 60.0) + 24)
+    else
+      format("%.2f", (overtime.hour - end_time.hour) + ((overtime.min - end_time.min) / 60.0)) 
+    end
+  end
   
- def format_overtime_m 
-   format("%2d", (time.min / 15) * 15)
- end
+ 
 
 end 
